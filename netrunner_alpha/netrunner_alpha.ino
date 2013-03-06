0233faff3e868da60f6da1442b8844537deb053a
virre <virre.annergard@gmail.com>
2013-03-06 14:31:21 +0100

    Netrunner tool alpha

--- netrunner_alpha/netrunner_alpha.ino
+++ netrunner_alpha/netrunner_alpha.ino
@@ -1,12 +1,18 @@
 
+/*
+  Netrunner (the LCG) click-tracker version 0.1
+  By: Virre Linwendil Annergård 2013
+  Feel free to share and edit and fix
+*/ 
 int led1=8;
 int led2=9;
 int led3=10;
 int led4=12;
 int but1=2;
-
+int counter=0;
+byte pressed;
+byte last_press;
 void setup() {
-  Serial.begin(9600);
   pinMode(led1,OUTPUT);
   pinMode(led2,OUTPUT);
   pinMode(led3,OUTPUT);
@@ -16,40 +22,45 @@ void setup() {
 }
 
 void loop() {
+  pressed=digitalRead(but1);
 
-  int clk=digitalRead(but1);
-  int counter=0;
-  if(clk==1) {
-    if (counter==4) {
-      digitalWrite(led1,LOW);
-      digitalWrite(led2,LOW);
-      digitalWrite(led3,LOW);
-      digitalWrite(led4,LOW);
+  if(pressed!=last_press) {
+    if(pressed==HIGH) {
+      switch(counter) {
+      case 0: 
+        digitalWrite(led1,HIGH); 
+        break;
+      case 1:
+        digitalWrite(led2,HIGH);
+        break;
+      case 2:
+        digitalWrite(led3,HIGH);
+        break;
+      case 3:
+        digitalWrite(led4,HIGH);
+        break;
+      case 4:
+        digitalWrite(led1,LOW);
+        digitalWrite(led2,LOW);
+        digitalWrite(led3,LOW);
+        digitalWrite(led4,LOW);
+
+      }
+      if(counter==4) {
+        counter=0; 
+      } 
+      else {
+        counter++;
+      }
     }
-    if(counter==0) {
-      digitalWrite(led1,HIGH);
-
-    }
-    if(counter==1) {
-      digitalWrite(led2,HIGH);
-
-    } 
-    if(counter==2) {
-      digitalWrite(led3,HIGH);
-
-    } 
-    if(counter==3) {
-      digitalWrite(led4,HIGH);
-
-    } 
-    counter=counter+1;
   }
 
 
-  if(clk==1) {
-    Serial.println(counter);
-  }
+  last_press=pressed;
 }
+
+
+
 
 
 
